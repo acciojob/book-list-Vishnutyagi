0f739e41b127d0d841997a151ade94845009ce37
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-06-21 15:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let submit=document.getElementById("submit");
+let n=0;
+submit.addEventListener('click',function(){
+	let title=document.getElementById("title").value;
+	let author=document.getElementById("author").value;
+	let isbn=document.getElementById("isbn").value;
+	let books=document.getElementById("book-list");
+	var row=document.createElement('tr');
+	row.innerHTML=`
+		<td>${title}</td>
+		<td>${author}</td>
+		<td>${isbn} <button id="${n}">x</button></td>
+	`
+	
+	books.appendChild(row);
+	document.getElementById(`${n}`).addEventListener('click',function(){
+		books.removeChild(row);
+	});
+	n++;
+});
